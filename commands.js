6877e49cda3b55f655d408897d4f40140bf2e379
Gustavo <Gustavoblancog29@gmail.com>
2021-12-01 14:16:37 -0400

    editada la informacion de start y agregada la ayuda

--- commands.js
+++ commands.js
@@ -3,10 +3,16 @@ module.exports = function(bot) {
   const si= require('./controllers/controllers.js')
 
   bot.start((ctx => {
-    ctx.reply('Bienvenidos, para enviar la informacion')
+    ctx.reply('Bienvenidos, para enviar la información se ejecuta con la siguiente estrucutra:')
+    ctx.reply('"/comisiones @alias, tarea, Cantidad"')
   }))
 
   bot.command('/comisiones',(ctx) => {
     si.split(ctx)
+  })
+
+  bot.command('/ayuda',(ctx) => {
+    ctx.reply('Para enviar la información se ejecuta con la siguiente estrucutra:')
+    ctx.reply('"/comisiones @alias, tarea, Cantidad"')
   })
 }
