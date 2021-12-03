ce0e83f511f3713c65932d6bfe9e903b8f18dbe0
Gustavo <Gustavoblancog29@gmail.com>
2021-12-02 20:36:56 -0400

    cambio de nombre, nuevo token, salto de linea

--- commands.js
+++ commands.js
@@ -3,8 +3,7 @@ module.exports = function(bot) {
   const si= require('./controllers/controllers.js')
 
   bot.start((ctx => {
-    ctx.reply('Bienvenidos, para enviar la información se ejecuta con la siguiente estrucutra:')
-    ctx.reply('"/comisiones @alias, tarea, Cantidad"')
+    ctx.reply('Bienvenidos, para enviar la información se ejecuta con la siguiente estrucutra: \n \n /tarea @alias, tarea, Cantidad')
   }))
 
   bot.command('/tarea',(ctx) => {
@@ -12,7 +11,6 @@ module.exports = function(bot) {
   })
 
   bot.command('/ayuda',(ctx) => {
-    ctx.reply('Para enviar la información se ejecuta con la siguiente estrucutra:')
-    ctx.reply('"/comisiones @alias, tarea, Cantidad"')
+    ctx.reply('Para enviar la información se ejecuta con la siguiente estrucutra: \n \n /tarea @alias, tarea, Cantidad')
   })
 }
